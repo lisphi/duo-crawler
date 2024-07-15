5e053a222cc058932d3992c0d3e5dad52455b716
Benja <lisphi@qq.com>
2024-07-15 22:01:45 +0800

    DuoCrawler

---
+++ main.py
@@ -0,0 +1,13 @@
+import duolingo
+
+
+def main():
+    crawler = duolingo.DuoCrawler()
+    # crawler.download_courses(False)
+    # crawler.download_story_static_files(False)
+    # crawler.generate_story_mp3_files(False)
+    # crawler.download_part_static_files(False)
+    # crawler.generate_part_mp3_files(False)
+
+if __name__ == '__main__':
+    main()
